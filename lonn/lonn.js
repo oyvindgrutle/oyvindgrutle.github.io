a43ab56ff707f92735e8fd62e599668074125b86
Øyvind Grutle <oyvindgrutle@yvinds-MacBook-Pro.local>
2018-03-12 09:37:06 +0100

    Bendik e slem

--- lonn/lonn.js
+++ lonn/lonn.js
@@ -4,13 +4,15 @@ function setup() {
     let inpKveld = document.getElementById("kveld");
     let btnBeregn = document.getElementById("beregn");
     let divTotal = document.getElementById("total");
+    let inpProsent = document.getElementById("prosent");
+    let inpTabell = document.getElementById("tabell")
 
     btnBeregn.addEventListener("click", beregn); //funksjonen beregn blir kjørt når knappen beregn blir trykket på
 
     let timelonn = 158.09;
     let helgetillegg = 28;
     let kveldstillegg = 10;
-    //let skattprosent = 0.05;
+    let skattprosent = 0.05;
 
     function beregn() {
 
@@ -22,30 +24,41 @@ function setup() {
         let b = antallTimerihelg * helgetillegg;
         let c = antallKveld * kveldstillegg;
         let total = a + b + c;
-        
-        // runder av total lønn til nærmeste hundre
-        let avrunda = Math.round(total / 100) * 100;
-        //skatt hentet fra tabell 7100, egen liste liste.js
-        let skatt = liste[avrunda];
 
-        
+
+        let skatt;
+        if (inpProsent.checked) {
+            // runder av total lønn til nærmeste hundre
+            let avrunda = Math.round(total / 100) * 100;
+            //skatt hentet fra tabell 7100, egen liste liste.js
+            skatt = liste[avrunda];
+        } else {
+            skatt = (total * skattprosent).toFixed(2);
+        }
+
+
 
         //skatt på 5%
         //let skatt = (total * skattprosent).toFixed(2);
 
         // if setning som sier at antall timer i helg og kveld til sammen ikke kan være større enn antall timer
         if (antallTimer < antallTimerihelg || antallTimer < antallKveld || antallTimer < (antallKveld + antallTimerihelg)) {
-            divTotal.innerHTML = "NB! Antall timer i helg og kveld til sammen kan ikke være større enn antall timer totalt. Antall timer denne måneden er satt til " + antallTimer + ". Timer i helg og kveld til sammen er " + (antallTimerihelg+antallKveld) + "."
+            divTotal.innerHTML = "NB! Antall timer i helg og kveld til sammen kan ikke være større enn antall timer totalt. Antall timer denne måneden er satt til " 
+            + antallTimer + ". Timer i helg og kveld til sammen er " 
+            + (antallTimerihelg + antallKveld) + "."
         } else {
-            divTotal.innerHTML = "Du min unge mann, denne måneden har du gjort deg fortjent til " + total.toFixed(2) + "kr. Godt jobbet. Du blir trekt " + skatt + "kr i skatt. Du får da " + (total - skatt).toFixed(2) + "kr til lønnskonto.";
+            divTotal.innerHTML = "Du min unge mann, denne måneden har du gjort deg fortjent til " 
+            + total.toFixed(2) + "kr. Godt jobbet. Du blir trekt " 
+            + skatt + "kr i skatt. Du får da " 
+            + (total - skatt).toFixed(2) + "kr til lønnskonto.";
 
         }
-        
+
         // Hvis lønn er så liten at du slipper å betale skatt
         //if (skatt === undefined) {
-         //   divTotal.innerHTML = "Du har denne måneden tjent " + total.toFixed(2) + "kr. Du betaler ingenting i skatt."; 
-       // }
-        
+        //   divTotal.innerHTML = "Du har denne måneden tjent " + total.toFixed(2) + "kr. Du betaler ingenting i skatt."; 
+        // }
+
 
     }
 
